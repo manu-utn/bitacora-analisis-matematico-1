f8c7aa66cf3ba092ed36392802b5f14559663009
Emmanuel <emmanuel.lazarte.012@gmail.com>
2022-08-19 21:17:16 -0300

    Añadido: Macro para sqrt[n]

--- texmacs-config/manu-macros.ts
+++ texmacs-config/manu-macros.ts
@@ -6,10 +6,14 @@
   <\show-preamble>
     <assign|limite|<macro|A|B|C|<below|lim|<arg|A>\<rightarrow\><arg|B>>
     <arg|C>>>
+
+    <assign|raiz|<macro|A|B|C|<sqrt|<arg|A><rsup|<arg|B>>|<arg|C>>>>
+
+    \;
   </show-preamble>
 
   <\ignore>
-    \;
+    <samp|>
   </ignore>
 </body>
 
